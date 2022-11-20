782184ca63d3eaef9ca26b893dbbfa1cd3b542fd
Tommylegen <102867229+Tommylegen@users.noreply.github.com>
2022-11-20 12:26:49 +0700

    Update general-menu.js

--- plugins/general-menu.js
+++ plugins/general-menu.js
@@ -13,8 +13,8 @@ Agar terap bisa di gunakan dan tidak ada kendala seperti
     let uptime = clockString(_uptime)
     let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
     let ar = ['list', 'menu']
-    let title = `*© PATRICK*`
-    let tmb = `CLICK HERE`
+    let title = `*© BOTCAHX*`
+    let tmb = `CLICK HERE ⎙`
     const sections = [ {
 	title: `Aktif Selama ${uptime} || Waktu ${time} WIB`,
 	rows: [
