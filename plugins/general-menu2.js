d899d94e0ca3e88b64b7e87b4bd643c5b2def9f4
Tommylegen <102867229+Tommylegen@users.noreply.github.com>
2022-11-20 08:49:45 +0700

    Update general-menu2.js

--- plugins/general-menu2.js
+++ plugins/general-menu2.js
@@ -63,7 +63,7 @@ const defaultMenu = {
 │ • Prefix Used: *[ %p ]*
 │ • Database: %rtotalreg dari %totalreg 
 │ • Memory Used: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-│ • Publik Apikey: https://botcahx.ddns.net
+│ • Publik Apikey: -
 ╰────
 ╭─ 「 *USER INFO* 」
 │ • Pengguna:  %name 
